f24d57d1e56fa11fcb4f2249f7f4eb6c7f544aa9
PrajwalLuitel <prajwalluitel@gmai.com>
2023-10-02 18:27:16 +0545

    initial dev completed

--- app/index.js
+++ app/index.js
@@ -10,6 +10,7 @@ import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components
 const Home = () => {
 
   const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
 
   return(
   <SafeAreaView style={{flex:1, backgroundColor: COLORS.lightWhite}}>
@@ -36,6 +37,13 @@ const Home = () => {
         }}
         >
           <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm = {setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`)
+              }
+            }}
           />
 
           <Popularjobs />
